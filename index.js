3cecf3555c51bbe3394a92b6ac2aed171b322071
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-12-20 16:10:35 -0800

    api test

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 
-// app.use(express.static());
+app.use(express.static(resolve(__dirname, 'client', 'dist')));
 
+
+require('./routes')(app);
 
 app.get('/', (req, res) => {
     res.send('<h1>Server running</h1>')
