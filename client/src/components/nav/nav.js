8346f78f341fb9e14a9d59be01880042a25f29e1
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-12-28 12:31:56 -0800

    completed initial nav bar and side nav functionality

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -1,4 +1,8 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import {Link} from 'react-router-dom';
+import SideNav from './side_nav';
+import './nav.scss'
+
 
 class Nav extends Component {
 
@@ -35,10 +39,60 @@ class Nav extends Component {
         ]
     }
 
+    renderSignOut(){
+       return <button className="btn waves-effect waves-light blue" onClick={() => console.log("clicked to sign out")}>Sign Out</button>
+    }
+
+
+    buildLinks(link){
+        return (
+            <li key={link.to} >
+                <Link to={link.to}>{link.text}</Link>
+            </li>
+        );
+    }
+
+    renderLinks = () => {
+        const auth = true;
+
+        let authLinks = [];
+        // let nonAuthLinks = [];
+
+        const{ auth: navAuth, common, nonAuth } = this.state;
+
+        const commonLinks = common.map(this.buildLinks)
+
+        if(auth){
+            authLinks= navAuth.map(this.buildLinks)
+            authLinks.push(<li className="sign-out" key="/sign-out">{this.renderSignOut()}</li>)
+        }else{
+            authLinks= nonAuth.map(this.buildLinks)
+        }
+
+        return [...commonLinks, ...authLinks];
+    }
+
+    
 
     render(){
         return(
-            <nav className="grey darken-2"></nav>
+            <Fragment>
+                <nav className="grey darken-2">
+                    <div className="nav-wrapper">
+                        <Link className="brand-logo" to="/">Ishop</Link>
+                        <Link to="#" data-target="side-nav" className="sidenav-trigger">
+                            <i className="material-icons">menu</i>
+                        </Link>
+                        <ul className="right hide-on-med-and-down">
+                            {this.renderLinks()}
+                        </ul>
+                    </div>
+
+                    
+                </nav>
+
+                <SideNav renderLinks={this.renderLinks}/>
+            </Fragment>
         )
     }
 }
